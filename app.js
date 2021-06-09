d81335cc401cd4464d0521d928a89b8049362a1c
soumik <soumikitis@gmail.com>
2021-06-09 16:46:43 +0530

    game logic complete

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 "use strict";
+
 let playerInfo = {
     player1: {
         id: 1,
-        name:'Player1',
+        name:'player1',
         score: 0,
         color: "#273342"
     },
     player2: {
         id: 2,
-        name:'Player2',
+        name:'player2',
         score: 0,
         color: "#ad5534"
     }
 }
-
 let game = false;
 let p1Card = document.querySelector('.player1-score');
 let p2Card = document.querySelector('.player2-score');
@@ -24,7 +24,8 @@ let input = document.querySelectorAll('input');
 let startBtn = document.querySelector('.start');
 let resetBtn = document.querySelector('.reset');
 let actvie = document.querySelector('.active');
-let debug = 0;
+let result = document.querySelectorAll('.result');
+let winner;
 
 let randNums = () => {
     let arr = [];
@@ -45,39 +46,25 @@ let numToBoxes = (numlist) => {
     });
 };
 
-let mainGame = () => {
-    boxes.forEach((box) => {
-        box.addEventListener('click', () => {
-            if (game) {
-                box.style.background = playerInfo[currentPlayer].color;
-                box.style.color = 'white';
-                box.firstElementChild.classList.remove('hide');
-                box.classList.add('noclick');
-                let val = parseInt(box.firstElementChild.innerHTML);
-                playerInfo[currentPlayer].score += val;
-                showScore();
-                winCheck();
-                switchPlayer();
-            }
-        })
-    });
+let showNames = () => {
+    document.querySelector('#player1-name').textContent = playerInfo.player1.name;
+    document.querySelector('#player2-name').textContent = playerInfo.player2.name;
 }
 
-mainGame();
-
-input.forEach((input) => {
-    input.addEventListener('keyup', () => {
-        let player = input.id.split("-")[0];
-        let playerName=document.querySelector(`#${player}-name`);
-        if (input.value === "") {
-            playerName.textContent = player;
-        } else {
-            playerInfo[player].name = input.value;
-            playerName.textContent = input.value;
-        }
+let collectNames = () => {
+    input.forEach((input) => {
+        input.addEventListener('keyup', () => {
+            let player = input.id.split("-")[0];
+            let playerName=document.querySelector(`#${player}-name`);
+            if (input.value === "") {
+                playerInfo[player].name = player;
+            } else {
+                playerInfo[player].name = input.value;
+            }
+            showNames();
+        });
     });
-});
-
+}
 
 let switchPlayer = () => {
     document.querySelector(`.${currentPlayer}-active`).classList.add('hide');
@@ -90,48 +77,48 @@ let switchPlayer = () => {
     document.querySelector(`.${currentPlayer}-active`).classList.remove('hide');
 }
 
-let startGame = () => {
-    game = true;
-    numToBoxes(randNums());
-    hideStart();
-    document.querySelector(`.${currentPlayer}-active`).classList.remove('hide');
-    input.forEach((input) => {
-        input.classList.add('hide');
-    })
+let showScore = () => {
+    p1Card.textContent = playerInfo.player1.score;
+    p2Card.textContent = playerInfo.player2.score;
 }
-
 
 let winCheck = () => {
     let p1 = playerInfo.player1.score;
     let p2 = playerInfo.player2.score;
-    let p1Name = playerInfo.player1.name;
-    let p2Name = playerInfo.player2.name;
 
     if (playerInfo[currentPlayer].score >= 11) {
         revealCards();
         hideActive();
-        game = false;
+        winner = currentPlayer;
     }
-    if (p1 - p2 >= 6 && p2 > 2) {
-        console.log(`${p1Name} has won...`);
+    if (p1 - p2 >= 6 && p2 > 3) {
         revealCards();
-        game = false;
+        hideActive();
+        winner = 'player1'
     }
-    if (p2 - p1 >= 6 && p1 > 2) {
-        console.log(`${p2Name} has won...`);
+    if (p2 - p1 >= 6 && p1 > 3) {
         revealCards();
-        game = false;
+        hideActive();
+        winner = 'player2'
     }
     if (p1 + p2 === 21) {
+        hideActive();
         if (p1 > p2) {
-            game = false;
+            winner = 'player1'
         } else {
-            game = false;
+            winner = 'player2'
         }
     }
 }
 
-
+let declare = () => {
+    if (winner !== undefined) {
+        let winCol = document.querySelector(`.${winner}-result`);
+        let input = document.querySelector('selector');
+        winCol.classList.remove('remove');
+        winCol.classList.remove('hide3');
+    }
+}
 
 let revealCards = () => {
     nums.forEach((num) => {
@@ -140,33 +127,37 @@ let revealCards = () => {
     })
 }
 
-let showScore = () => {
-    p1Card.textContent = playerInfo.player1.score;
-    p2Card.textContent = playerInfo.player2.score;
-}
-
-let reset = () => {
-    game = false;
+let startGame = () => {
+    game = true;
     numToBoxes(randNums());
-    playerInfo.player1.score = 0;
-    playerInfo.player2.score = 0;
-    p1Card.textContent = 0;
-    p2Card.textContent = 0;
-
-    hideReset();
-
-    boxes.forEach((box) => {
-        box.style.background = "#dde8eb";
-        box.style.color = "black";
-        box.classList.remove('noclick');
-    })
-
+    hideStart();
+    document.querySelector(`.${currentPlayer}-active`).classList.remove('hide');
     input.forEach((input) => {
-        input.classList.remove('hide');
+        input.classList.add('hide3');
+        setTimeout(() => {
+            input.classList.add('remove');
+            result.forEach((result) => {
+                result.classList.remove('remove');
+            })
+        }, 500)
     })
-    hideActive();
+
 }
 
+let hiReShIn = () => {
+    result.forEach((result) => {
+        result.classList.add('hide3');
+        setTimeout(() => {
+            result.classList.add('remove');
+            input.forEach((input) => {
+                input.classList.remove('remove');
+                setTimeout(() => {
+                    input.classList.remove('hide3');
+                }, 100)
+            })
+        }, 500)
+    })
+}
 
 let hideStart = () => {
     startBtn.classList.add('hide2');
@@ -196,26 +187,80 @@ let hideActive = () => {
     }, 50)
 }
 
-if (game === false) {
-    hideActive();
+let resetBoxes = () => {
+    boxes.forEach((box) => {
+        box.style.background = "#dde8eb";
+        box.style.color = "black";
+        box.classList.remove('noclick');
+    })
 }
+
+let resetNames = () => {
+    playerInfo.player1.name = 'player1';
+    playerInfo.player2.name = 'player2';
+    input.forEach((input) => {
+        input.value = "";
+    })
+
+}
+
+let masterReset = () => {
+    game = false;
+    numToBoxes(randNums());
+    playerInfo.player1.score = 0;
+    playerInfo.player2.score = 0;
+    p1Card.textContent = 0;
+    p2Card.textContent = 0;
+
+    resetNames();
+    showNames();
+    hideReset();
+    resetBoxes();
+    hiReShIn();
+    hideActive();
+
+    setTimeout(() => {
+        currentPlayer = "player1";
+    }, 500)
+
+    winner = undefined;
+}
+
+let mainGame = () => {
+    collectNames();
+    showNames();
+    boxes.forEach((box) => {
+        box.addEventListener('click', () => {
+            if (game) {
+                box.style.background = playerInfo[currentPlayer].color;
+                box.style.color = 'white';
+                box.firstElementChild.classList.remove('hide');
+                box.classList.add('noclick');
+                let val = parseInt(box.firstElementChild.innerHTML);
+                playerInfo[currentPlayer].score += val;
+                setTimeout(() => {
+                    showScore();
+                    winCheck();
+                    declare();
+                    switchPlayer();
+                }, 600)
+            }
+            if (game === false) {
+                setTimeout(() => {
+                    hideActive();
+                }, 700)
+            }
+        })
+    });
+}
+
+mainGame();
+
+
+
 
 
 startBtn.addEventListener('click', startGame);
 startBtn.addEventListener('mouseenter', () => {startBtn.classList.add('animate')});
 startBtn.addEventListener('mouseleave', () => {startBtn.classList.remove('animate')});
-resetBtn.addEventListener('click', reset);
-
-
-
-
-
-
-
-// console.log(playerInfo.player1.score)
-
-// generate random unique numbers upto 6
-
-// set current player
-
-// assign the random unique numbers to the divs
+resetBtn.addEventListener('click', masterReset);
